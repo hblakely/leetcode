5c761698cd61277153a5974601327b4a54f65bb5
Hunter Blakely <764395+hblakely@users.noreply.github.com>
2022-11-27 16:42:44 -0800

    Altered access modifiers for ListNode.java

--- src/helper/ListNode.java
+++ src/helper/ListNode.java
@@ -2,8 +2,8 @@ package helper;
 
 // Definition for singly-linked list.
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
     public ListNode() {}
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
