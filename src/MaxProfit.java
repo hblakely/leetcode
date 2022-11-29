400e26c9f9bc0b73aafbc6b47639dba139460ad9
Hunter Blakely <764395+hblakely@users.noreply.github.com>
2022-11-28 19:26:19 -0800

    Find greatest profit in array of prices where index is the day. Brute force was slow. O(n) solution found.

---
+++ src/MaxProfit.java
@@ -0,0 +1,41 @@
+public class MaxProfit {
+    /*
+    You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+    You want to maximize your profit by choosing a single day to buy one stock and choosing a different
+    day in the future to sell that stock.
+
+    Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+     */
+
+    public int maxProfit(int[] prices) { // Iterate through prices once - O(n) complexity
+        int profit = 0;
+        if(prices.length <2) // invalid input
+            return profit;
+
+        int lowest = prices[0]; // keep track of lowest price and compare difference of current price with lowest
+        for(int i = 1; i<prices.length; i++){
+            int diff = prices[i]-lowest;
+            if(diff>profit) profit = diff; // update profit if larger profit is found
+
+            if(prices[i]<lowest)
+                lowest = prices[i];
+        }
+        return profit;
+    }
+
+
+    public int maxProfitFirstAttempt(int[] prices) {
+        int profit =0;
+        if(prices.length <2) // invalid input
+            return profit;
+        for(int i = 0; i<prices.length; i++){ // N^2 suckkkkksssssss..... WAY slow.
+            for(int j=i+1; j<prices.length; j++){
+                int val = prices[j]-prices[i];
+                profit = val>profit?val:profit;
+            }
+        }
+
+        return profit;
+    }
+}
